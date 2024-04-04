4102359b7b8c8bef919100f3ed9b0195d352b5f4
Shujaa <shujero.service@gmail.com>
2024-04-04 16:04:25 +0300

    first commit

---
+++ src/api/siel-blog/routes/siel-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * siel-blog router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::siel-blog.siel-blog');
---
+++ src/api/siel-blog/services/siel-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * siel-blog service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::siel-blog.siel-blog');
