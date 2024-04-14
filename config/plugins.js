6578c378d07e10195e2aa50ac9f7de6bf99fa835
Shujaa <shujero.service@gmail.com>
2024-04-14 14:42:45 +0300

    updatedplugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+const crypto = require('crypto');
+
+module.exports = () => ({
+  // Other plugin configurations...
+  'users-permissions': {
+    jwtSecret: crypto.randomBytes(16).toString('base64')
+  },
+});
