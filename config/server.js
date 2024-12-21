c0305a6df9e38b7df08963db532e2e67c09d9aae
Shujero <shujero.service@gmail.com>
2024-12-21 14:03:15 +0300

    mods

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['fajncjnchnsincrusn']),
+    url: 'http://46.202.143.190:1337',
   },
   admin: {
     auth: {
