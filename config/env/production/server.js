1f70801700e359d6b1b3979e96365981074d30fe
Shujero <shujero.service@gmail.com>
2024-12-21 14:34:09 +0300

    mods

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     proxy: true,
     host: "0.0.0.0",
     port: process.env.PORT,
-    url: env('https://siel-strapi.onrender.com'),
+    url: env('http://46.202.143.190:1337'),
     app: { 
       keys: env.array('APP_KEYS')
     },
